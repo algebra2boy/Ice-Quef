3e7e962ef14ab9210f2550f30f0f2622f253601b
Kolyn090 <78561131+Kolyn090@users.noreply.github.com>
2024-02-14 21:08:43 -0500

    Added basic UI components

--- Ice_Quef/App.js
+++ Ice_Quef/App.js
@@ -1,22 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { ThemeProvider, ThemeContext } from './kits/AppTheme';
+import { setNavigatorTabIndex, getNavigatorTabIndex } from './props/NavigatorTabIndexController';
 
-//
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+  state = {
+    isReady: false,
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Please Standby!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Login"
+          screenOptions={{headerShown: false, gestureEnabled: false}}
+        >
+          
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+function ThemedStyles() {
+  const themeManager = React.useContext(ThemeContext);
+  const currentTheme = themeManager.theme;
+
+  return (StyleSheet.create({
+
+    bottomCircle: {
+      top: -45,
+      width: 80,
+      height: 80,
+      borderRadius: 40,
+      alignSelf: 'center',
+      backgroundColor: currentTheme.mainColor
+    },
+  
+    bottomUnderline: {
+      top: -25,
+      width: 48,
+      height: 5,
+      borderRadius: 5,
+      backgroundColor: currentTheme.primaryColor,
+      alignSelf: 'center'
+    },
+  
+    smallPageIcon: {
+      resizeMode: 'contain',
+      width: 56,
+      height: 56,
+      top: -30,
+    },
+  
+    bigPageIcon: {
+      top: 10,
+      resizeMode: 'contain',
+      width: 64,
+      height: 64,
+      alignSelf: 'center'
+    },
+  
+  }));
+}
