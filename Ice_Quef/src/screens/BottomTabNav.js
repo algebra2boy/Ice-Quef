3b601bc7b2456cf6ecc86ae3f24446bfed469ac2
Kolyn090 <78561131+Kolyn090@users.noreply.github.com>
2024-02-15 21:17:25 -0500

    Added more UI

--- Ice_Quef/src/screens/BottomTabNav.js
+++ Ice_Quef/src/screens/BottomTabNav.js
@@ -2,55 +2,50 @@ import { ThemeContext } from '../kit/AppTheme';
 import { StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-
 const Tab = createBottomTabNavigator();
 
 function BottomTabNavigator() {
-    const themedStyles = ThemedStyles();
-    
-    
+  const themedStyles = ThemedStyles();
 }
 
 function ThemedStyles() {
   const themeManager = React.useContext(ThemeContext);
   const currentTheme = themeManager.theme;
 
-  return (StyleSheet.create({
-
+  return StyleSheet.create({
     bottomCircle: {
       top: -45,
       width: 80,
       height: 80,
       borderRadius: 40,
       alignSelf: 'center',
-      backgroundColor: currentTheme.mainColor
+      backgroundColor: currentTheme.mainColor,
     },
-  
+
     bottomUnderline: {
       top: -25,
       width: 48,
       height: 5,
       borderRadius: 5,
       backgroundColor: currentTheme.primaryColor,
-      alignSelf: 'center'
+      alignSelf: 'center',
     },
-  
+
     smallPageIcon: {
       resizeMode: 'contain',
       width: 56,
       height: 56,
       top: -30,
     },
-  
+
     bigPageIcon: {
       top: 10,
       resizeMode: 'contain',
       width: 64,
       height: 64,
-      alignSelf: 'center'
+      alignSelf: 'center',
     },
-  
-  }));
+  });
 }
 
 export default BottomTabNavigator;
--- Ice_Quef/src/screens/LoginPage.js
+++ Ice_Quef/src/screens/LoginPage.js
@@ -1,33 +1,23 @@
 import React, { useState } from 'react';
-import { 
-  SafeAreaView, 
-  StyleSheet, 
-  Text, 
-  Image, 
-  View, 
-  Dimensions 
-} from 'react-native';
+import { SafeAreaView, StyleSheet, TextInput, Image, View, Dimensions } from 'react-native';
 import * as SplashScreen from 'expo-splash-screen';
 import loadFont from '../props/FontLoader';
-import Logo from '../../assets/images/Logo.png';
-import Title from '../../assets/images/Title.png'
-import CustomTextInput from '../component/customInput/CutomTextInput';
-import CustomButton from '../component/customButton/CustomButton';
-import SocialSignInButtons from '../component/socialSignInButton/SocialSignInButton';
+import Title from '../../assets/images/3D_Logo.png';
+import { SpringButton } from '../kit/SpringButton';
+import * as KolynStyle from '../kit/KolynStyleKit';
+import { ThemeContext } from '../kit/AppTheme';
 
+const { width } = Dimensions.get('window');
 
-function LoginPage({ navigation }) {
+export function LoginPage({ navigation }) {
+  const themedStyles = ThemedStyles();
+
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const {width, height} = Dimensions.get('window');
 
   const onSignInPressed = () => {
     // validate user
     navigation.navigate('Home');
-  };
-
-  const onForgotPasswordPressed = () => {
-    navigation.navigate('ForgotPassword');
   };
 
   const onSignUpPress = () => {
@@ -46,61 +36,116 @@ function LoginPage({ navigation }) {
   }
 
   return (
-    <View onLayout={onLayoutRootView}>
-      <SafeAreaView style={styles.root}>
-        <Image 
-          source={Title}
-          style={[{
-            width: '75%',
-            maxWidth: 256,
-            height: width * 0.75,
-            maxHeight: 256
-          }]}
-          resizeMode="contain"
-        />
+    <View onLayout={onLayoutRootView} style={themedStyles.screen}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={{ flex: 1 }}>
+          <Image
+            source={Title}
+            style={[
+              {
+                width: '75%',
+                maxWidth: 256,
+                height: width * 0.75,
+                maxHeight: 256,
+                alignSelf: 'center',
+              },
+            ]}
+            resizeMode="contain"
+          />
+        </View>
 
-        <CustomTextInput 
-          placeholder="User name"
-          value={username}
-          setValue={setUsername}
-        />
-        <CustomTextInput 
-          placeholder="Password"
-          value={password}
-          setValue={setPassword}
-          secureTextEntry={true}
-        />
+        <View style={{ flex: 1, alignItems: 'center' }}>
+          <EmailTextfield
+            emailText={username}
+            onChangeEmailText={setUsername}
+            textfieldStyle={themedStyles.inputTextfield}
+          />
 
-        <CustomButton 
-          text="Sign In" 
-          onPress={onSignInPressed}
-        />
+          <PasswordTextfild
+            passwordText={password}
+            onChangePasswordText={setPassword}
+            textfieldStyle={themedStyles.inputTextfield}
+          />
+        </View>
 
-        <CustomButton
-          text="Forgot password?"
-          onPress={onForgotPasswordPressed}
-          type="TERTIARY"
-        />
-
-        <SocialSignInButtons />
-
-        <CustomButton
-          text="Don't have an account? Create one"
-          onPress={onSignUpPress}
-          type="TERTIARY"
-        />
+        <View style={{ flex: 1, alignItems: 'center' }}>
+          <SpringButton
+            text="Log In"
+            onPress={onSignInPressed}
+            buttonStyle={themedStyles.casualButton}
+            labelStyle={themedStyles.casualButtonLabel}
+          />
+          <SpringButton
+            text="Sign Up"
+            onPress={onSignUpPress}
+            buttonStyle={[themedStyles.casualButton, { top: 30 }]}
+            labelStyle={themedStyles.casualButtonLabel}
+          />
+        </View>
       </SafeAreaView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+/* The email textfield */
+function EmailTextfield({ emailText, onChangeEmailText, textfieldStyle }) {
+  return (
+    <TextInput
+      style={textfieldStyle}
+      value={emailText}
+      onChangeText={onChangeEmailText}
+      placeholder="Enter email"
+      keyboardType="email-address"
+      secureTextEntry={false}
+    />
+  );
+}
 
+/* The password textfield, secure typing */
+function PasswordTextfild({ onChangePasswordText, passwordText, textfieldStyle }) {
+  return (
+    <TextInput
+      style={textfieldStyle}
+      value={passwordText}
+      onChangeText={onChangePasswordText}
+      placeholder="Enter password"
+      keyboardType="default"
+      secureTextEntry={true}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
   root: {
     alignItems: 'center',
     padding: 20,
   },
-
 });
 
-export default LoginPage;
+function ThemedStyles() {
+  const themeManager = React.useContext(ThemeContext);
+  const currentTheme = themeManager.theme;
+
+  return StyleSheet.create({
+    screen: StyleSheet.flatten([KolynStyle.kolynScreen(currentTheme.backgroundColor)]),
+
+    inputTextfield: StyleSheet.flatten([
+      { height: 40, width: 300 },
+      KolynStyle.kolynInputTextfield(currentTheme.primaryColor, currentTheme.mainFont),
+    ]),
+
+    casualButton: StyleSheet.flatten([
+      { height: 40, width: 200, backgroundColor: currentTheme.mainColor, alignSelf: 'center' },
+      KolynStyle.kolynButton(currentTheme.mainColor),
+    ]),
+
+    casualButtonLabel: StyleSheet.flatten([
+      { backgroundColor: currentTheme.mainColor },
+      KolynStyle.kolynLabel(
+        currentTheme.fontSizes.casual,
+        currentTheme.mainFont,
+        currentTheme.primaryColor,
+      ),
+    ]),
+  });
+}
