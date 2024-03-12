12f3d68f8a8f2be8b1f36483ffb1ed14ccdf150e
Kolyn090 <78561131+Kolyn090@users.noreply.github.com>
2024-03-11 22:38:45 -0400

    More MVC modularization

---
+++ Ice_Quef/src/controllers/loginpage/DefaultController.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { LoginPageDefault } from '../../views/loginpage/Default'
+
+export function LoginPageDefaultController() {
+  return (
+    <LoginPageDefault />
+  );
+}
