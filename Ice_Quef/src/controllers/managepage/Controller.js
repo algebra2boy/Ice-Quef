372350478f51674a94e1e9b73c074407d69cc862
JianxinLin28 <jianxinlin@umass.edu>
2024-03-15 19:43:19 -0400

    Manage page: Add confirm, Add success, Add fail

--- Ice_Quef/src/controllers/managepage/Controller.js
+++ Ice_Quef/src/controllers/managepage/Controller.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { ManagePageDefaultController } from './DefaultController';
 import { ManagePageAddOHController } from './AddOHController';
+import { ManagePageAddConfirm } from '../../views/managepage/AddConfirm';
+import { ManagePageAddSuccess } from '../../views/managepage/AddSuccess';
+import { ManagePageAddFail } from '../../views/managepage/AddFail';
 
 
 const Stack = createNativeStackNavigator();
@@ -15,6 +18,9 @@ export function ManagePageController() {
 
       <Stack.Screen name="ManagePageDefault" component={ManagePageDefaultController}/>
       <Stack.Screen name="ManagePageAddOH" component={ManagePageAddOHController}/>
+      <Stack.Screen name="ManagePageAddConfirm" component={ManagePageAddConfirm}/>
+      <Stack.Screen name="ManagePageAddSuccess" component={ManagePageAddSuccess}/>
+      <Stack.Screen name="ManagePageAddFail" component={ManagePageAddFail}/>
 
     </Stack.Navigator>
   );
