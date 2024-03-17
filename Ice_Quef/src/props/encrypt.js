b72c4d802401c6fae9b4e70b8d267f15c1a34ea9
Haijie <64161293+ygbull@users.noreply.github.com>
2024-03-16 20:36:54 -0400

    add basic signup and login request

--- Ice_Quef/src/props/encrypt.js
+++ Ice_Quef/src/props/encrypt.js
@@ -1,14 +1,9 @@
+import SHA256 from 'crypto-js/sha256';
+
 async function encryptPassword(password) {
-    // string to an array buffer
-    const encoder = new TextEncoder();
-    const data = encoder.encode(password);
-
-    // sha256
-    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
-
-    // array buffer to a hex string
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
-
-    return hashHex;
+    const hashWordArray = SHA256(password);
+     // hash to a hex string
+    return hashWordArray.toString();
 }
+
+export default encryptPassword;
