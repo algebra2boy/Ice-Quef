e52c86bfd2c7eda62e4cdbc1ec1bd9ba126dea8a
JianxinLin28 <jianxinlin@umass.edu>
2024-03-23 19:26:43 -0400

    Moved logic from View to Controller

--- Ice_Quef/src/views/calendar_page/Default.js
+++ Ice_Quef/src/views/calendar_page/Default.js
@@ -5,31 +5,17 @@ import { BasePage } from '../../style/BasePage';
 import { ThemeContext } from '../../style/AppTheme';
 import { renderEvent, eventCellTheme } from '../../style/CaleStyle';
 import * as KolynStyle from '../../style/KolynStyleKit';
-import { UserContext } from '../../props/UserInfo';
 
-export function CalendarPageDefault({ registered }) {
+export function CalendarPageDefault(props) {
   const themedStyles = ThemedStyles();
   const hourStyle = getHourStyle();
 
-  const lst = getRenderList(registered);
-
   const [isOpenMenu, setOpenMenu] = useState(false);
   const [title, setTitle] = useState('');
-  const [message, setMessage] = useState('Press to join waitlist.');
 
-  const userManager = React.useContext(UserContext);
-
-  const onPressEvent = (event) => {
+  const onPressEvent = event => {
     setOpenMenu(true);
     setTitle(event.title);
-  };
-
-  const determineMessage = () => {
-    if (message == 'Press to join waitlist.') {
-      setMessage('Your current position: 4');
-    } else {
-      setMessage('Press to join waitlist.');
-    }
   };
 
   return (
@@ -38,7 +24,7 @@ export function CalendarPageDefault({ registered }) {
         <>
           <View style={{ flex: 20 }}>
             <Calendar
-              events={lst}
+              events={props.regLst}
               height={400}
               mode="3days"
               theme={eventCellTheme()}
@@ -48,29 +34,12 @@ export function CalendarPageDefault({ registered }) {
               onPressEvent={onPressEvent}
             />
             {isOpenMenu && (
-              <View style={themedStyles.bottomOverlay}>
-                <View style={themedStyles.topOverlay}>
-                  <View style={{ bottom: '10%' }}>
-                    <TouchableOpacity
-                      style={themedStyles.closeMenuBotton}
-                      onPress={() => setOpenMenu(false)}
-                    >
-                      <CrossMark />
-                    </TouchableOpacity>
-
-                    <Text style={[themedStyles.waitlistHintLabel]}>
-                      {title}
-                    </Text>
-
-                    <TouchableOpacity
-                      style={themedStyles.waitlistButton}
-                      onPress={() => determineMessage()}
-                    >
-                      <Text style={themedStyles.waitlistLabel}>{message}</Text>
-                    </TouchableOpacity>
-                  </View>
-                </View>
-              </View>
+              <PopupMenu
+                title={title}
+                determineMessage={props.determineMessage}
+                message={props.message}
+                setOpenMenu={setOpenMenu}
+              />
             )}
           </View>
         </>
@@ -78,113 +47,6 @@ export function CalendarPageDefault({ registered }) {
     />
   );
 }
-
-/**
- * Convert a list of registered office hours into
- * renderable info.
- * 
- * @param {*} registered a list of registered office hours
- */
-const getRenderList = (registered) => {
-  /**
-   * Determine the info needed for the 'blocks'.
-   * It will return a list because oh is repeated. 
-   * 
-   * @param {object} oh an office hour
-   */
-  const getRenderInfo = (oh) => {
-    const getDate = (myDate) => {
-      const date = myDate.split(':');
-      var month = parseInt(date[0])-1;
-      var day = parseInt(date[1]);
-      var year = parseInt(date[2]);
-
-      return new Date(year, month, day);
-    };
-
-    const getTime = (myTime) => {
-      const time = myTime.split(':');
-      var hour = parseInt(time[0]);
-      var minute = parseInt(time[1]);
-
-      return [hour, minute];
-    };
-
-    const getWeekDay = () => {
-      return oh.day;
-    };
-
-    const addDays = (date, addition) => {
-      var result = new Date(date);
-      result.setDate(result.getDate() + addition);
-      return result;
-    }
-
-    const hasReachTerminal = (curr, terminal) => {
-      return curr.getFullYear() > terminal.getFullYear() ||
-              (curr.getFullYear() === terminal.getFullYear() &&
-              curr.getMonth() > terminal.getMonth()) ||
-              (curr.getFullYear() === terminal.getFullYear() &&
-                curr.getMonth() === terminal.getMonth() &&
-                curr.getDate() > terminal.getDate());
-    }
-
-    const getTitle = () => {
-      const getCourse = () => {
-        return oh.courseDepartment + " " + oh.courseNumber;
-      };
-  
-      const getFacultyName = () => {
-        return oh.facultyName;
-      };
-
-      return getCourse() + ", " + getFacultyName();
-    };
-
-    const initialDate = getDate(oh.initialDate);
-    const terminalDate = getDate(oh.terminalDate);
-    const startTime = getTime(oh.startTime);
-    const endTime = getTime(oh.endTime);
-    const weekDay = getWeekDay();
-    const title = getTitle();
-    
-    const info = [];
-    var currentDate;
-    const convertToCorrectStartDate = (curr, weekDay) => {
-      var currDay = curr.getDay();
-      if (currDay > weekDay) {
-        return addDays(curr, 7-(currDay-weekDay));
-      } 
-      else if (currDay < weekDay) {
-        return addDays(curr, weekDay-currDay);
-      }
-      else {
-        return curr;
-      }
-    }
-    for (currentDate = convertToCorrectStartDate(initialDate, weekDay); 
-        !hasReachTerminal(currentDate, terminalDate); 
-        currentDate = addDays(currentDate, 7)) {
-        info.push(
-        {
-          title: title,
-          start: new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), startTime[0], startTime[1]),
-          end: new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), endTime[0], endTime[1]),
-        }
-      );
-    }
-
-    return info;
-  };
-
-  const result = [];
-  for (var i = 0; i < registered.length; i++) {
-    const renderInfo = getRenderInfo(registered[i]);
-    result.push(renderInfo);
-  }
-
-  return result.flat();
-};
 
 const getHourStyle = () => {
   const themeManager = React.useContext(ThemeContext);
@@ -196,6 +58,34 @@ const getHourStyle = () => {
     color: currentTheme.mainColor,
   };
 };
+
+function PopupMenu(props) {
+  const themedStyles = ThemedStyles();
+
+  return (
+    <View style={themedStyles.bottomOverlay}>
+      <View style={themedStyles.topOverlay}>
+        <View style={{ bottom: '10%' }}>
+          <TouchableOpacity
+            style={themedStyles.closeMenuBotton}
+            onPress={() => props.setOpenMenu(false)}
+          >
+            <CrossMark />
+          </TouchableOpacity>
+
+          <Text style={[themedStyles.waitlistHintLabel]}>{props.title}</Text>
+
+          <TouchableOpacity
+            style={themedStyles.waitlistButton}
+            onPress={() => props.determineMessage()}
+          >
+            <Text style={themedStyles.waitlistLabel}>{props.message}</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
 
 function CrossMark() {
   const themedStyles = ThemedStyles();
