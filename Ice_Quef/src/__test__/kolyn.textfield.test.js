d11c357879ea93c4f9064a07f68dbcf23930660c
Kolyn090 <78561131+Kolyn090@users.noreply.github.com>
2024-03-24 00:27:40 -0400

    Added the first test

---
+++ Ice_Quef/src/__test__/kolyn.textfield.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { KolynTextfield } from '../component';
+import { ThemeProvider } from '../style/AppTheme';
+
+
+describe('Textfield', () => {
+  it('puts text', async () => {
+  const tree = renderer.create(
+    <ThemeProvider>
+      <KolynTextfield />
+    </ThemeProvider>
+  ).toJSON();
+
+  console.log(tree);
+  });
+});
