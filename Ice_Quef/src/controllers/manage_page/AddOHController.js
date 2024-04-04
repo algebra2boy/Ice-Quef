06b9365e35fc5665d7d65ebef5f36e60127e6b3d
Haijie <64161293+ygbull@users.noreply.github.com>
2024-04-04 15:22:33 -0400

    add search office hour function

--- Ice_Quef/src/controllers/manage_page/AddOHController.js
+++ Ice_Quef/src/controllers/manage_page/AddOHController.js
@@ -1,35 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import { ManagePageAddOH } from '../../views/manage_page/AddOH';
-import { FetchOfficeHours } from '../../models/OfficeHourSearcher';
-import { LoadingPage } from '../../component/LoadingPage';
+import React, {useState, useEffect} from 'react';
+import {ManagePageAddOH} from '../../views/manage_page/AddOH';
+import {LoadingPage} from '../../component/LoadingPage';
+import {PerformSearch} from "../../models/OfficeHourSearcher";
 
 export function ManagePageAddOHController() {
-  const [officeHour, setOfficeHour] = useState([]);
-  const [isLoading, setIsLoading] = useState(true); // loading indicator
-  const [userInput, setUserInput] = useState('');
+    const [officeHour, setOfficeHour] = useState([]);
+    const [isLoading, setIsLoading] = useState(true); // loading indicator
+    const [userInput, setUserInput] = useState('');
 
-  // Todo: set office hour list whenever user input changes
+    // Todo: set office hour list whenever user input changes
 
-  useEffect(() => {
-    const fetchUserOfficeHour = async () => {
-      try {
-        setIsLoading(true); // Before the fetch starts
-        //const officeHours = await FetchOfficeHours(''); // attempting to fetch without email - this is wrong
-        //setOfficeHour(officeHours);
-        // console.log(officeHours);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setIsLoading(false); // fetch is complete or if there is an error
-      }
-    };
+    useEffect(() => {
+        const fetchUserOfficeHour = async () => {
+            try {
+                console.log(userInput)
+                setIsLoading(true); // Before the fetch starts
+                const fetchedOH = await PerformSearch(userInput)
+                console.log(fetchedOH)
+                setOfficeHour(fetchedOH)
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setIsLoading(false); // fetch is complete or if there is an error
+            }
+        };
 
-    fetchUserOfficeHour();
-  }, []);
+        fetchUserOfficeHour();
+    }, [userInput]);
 
-  if (isLoading) {
-    return <LoadingPage text="Loading office hours..." />;
-  }
 
-  return <ManagePageAddOH ohList={officeHour} text={userInput} setText={setUserInput} />;
+    if (isLoading) {
+        return <LoadingPage text="Loading office hours..."/>;
+    }
+
+
+    return <ManagePageAddOH ohList={officeHour} text={userInput} setText={setUserInput}/>;
 }
