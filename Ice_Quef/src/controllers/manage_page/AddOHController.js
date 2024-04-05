34d659eb99e99d4f61ea95da6b04052a2db883d4
Kolyn090 <78561131+Kolyn090@users.noreply.github.com>
2024-04-05 14:10:33 -0400

    Now searching only renders the component it needs to. Also set the searching span to 1 second

--- Ice_Quef/src/controllers/manage_page/AddOHController.js
+++ Ice_Quef/src/controllers/manage_page/AddOHController.js
@@ -1,70 +1,76 @@
-import React, {useState, useEffect} from 'react';
-import {ManagePageAddOH} from '../../views/manage_page/AddOH';
-import {LoadingPage} from '../../component/LoadingPage';
-import {PerformSearch} from '../../models/OfficeHourSearcher';
+import React, { useState, useEffect } from 'react';
+import { ManagePageAddOH } from '../../views/manage_page/AddOH';
+import { LoadingPage } from '../../component/LoadingPage';
+import { PerformSearch } from '../../models/OfficeHourSearcher';
 
 function debounce(func, wait) {
-    let timeout;
+  let timeout;
 
-    // debounce return function
-    const executedFunction = function (...args) {
-        const later = () => {
-            clearTimeout(timeout);
-            func(...args);
-        };
-
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
+  // debounce return function
+  const executedFunction = (...args) => {
+    const later = () => {
+      clearTimeout(timeout);
+      func(...args);
     };
 
-    // cancel method to clear the timeout after
-    executedFunction.cancel = function () {
-        clearTimeout(timeout);
-    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
 
-    return executedFunction;
+  // cancel method to clear the timeout after
+  executedFunction.cancel = () => {
+    clearTimeout(timeout);
+  };
+
+  return executedFunction;
 }
 
-
 export function ManagePageAddOHController() {
-    const [officeHour, setOfficeHour] = useState([]);
-    const [isLoading, setIsLoading] = useState(true); // initial loading indicator
-    const [isSearching, setIsSearching] = useState(false);
-    const [courseCode, setCourseCode] = useState('');
-    const [facultyName, setFacultyName] = useState('');
+  const [officeHour, setOfficeHour] = useState([]);
 
-    const debouncedSearchResult = debounce(async () => {
-        if (!isSearching) setIsSearching(true); // true if not already searched
-        try {
-            const fetchedOH = await PerformSearch(facultyName, courseCode);
-            setOfficeHour(fetchedOH);
-        } catch (error) {
-            console.error(error);
-        } finally {
-            setIsSearching(false);
-            setIsLoading(false);
-        }
-    }, 3000); // 3000 milliseconds, 3 seconds
+  const [isSearching, setIsSearching] = useState(false);
+  const [courseCode, setCourseCode] = useState('');
+  const [facultyName, setFacultyName] = useState('');
+  // The refresh control for the course flat list
+  const [isRefreshing, setIsRefreshing] = useState(false);
 
-    useEffect(() => {
-        debouncedSearchResult();
-        return () => debouncedSearchResult.cancel();
-    }, [courseCode, facultyName]);
-
-    if (isLoading) {
-        return <LoadingPage text="Loading office hours..."/>;
-    } else if (isSearching) {
-        return <LoadingPage text="Searching..."/>;
+  const debouncedSearchResult = debounce(async () => {
+    if (!isSearching) {
+      setIsSearching(true); // true if not already searched
     }
 
-    return (
-        <ManagePageAddOH
-            officeHour={officeHour}
-            setOfficeHour={setOfficeHour}
-            courseCode={courseCode}
-            setCourseCode={setCourseCode}
-            facultyName={facultyName}
-            setFacultyName={setFacultyName}
-        />
-    );
+    // nothing entered yet
+    if (courseCode === '' && facultyName === '') {
+      setOfficeHour([]);
+    } else {
+      // entered something, start searching
+      try {
+        const fetchedOH = await PerformSearch(facultyName, courseCode);
+        setOfficeHour(fetchedOH);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsSearching(false);
+      }
+    }
+  }, 1000); // 1000 milliseconds, 3 seconds
+
+  useEffect(() => {
+    debouncedSearchResult();
+    return () => debouncedSearchResult.cancel();
+  }, [courseCode, facultyName]);
+
+  return (
+    <ManagePageAddOH
+      isRefreshing={isRefreshing}
+      setIsRefreshing={setIsRefreshing}
+      isSearching={isSearching}
+      officeHour={officeHour}
+      setOfficeHour={setOfficeHour}
+      courseCode={courseCode}
+      setCourseCode={setCourseCode}
+      facultyName={facultyName}
+      setFacultyName={setFacultyName}
+    />
+  );
 }
