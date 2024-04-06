83b4b4df7e3ab0ff24b51e1e75a12169abbc537b
Haijie <64161293+ygbull@users.noreply.github.com>
2024-04-05 20:00:05 -0400

    Add the add user office hour

---
+++ Ice_Quef/src/controllers/manage_page/AddConfirmController.js
@@ -0,0 +1,26 @@
+import ServerAddress from "../../props/Server";
+
+export async function addUserOfficeHour(userEmail, userToken, officeHourID) {
+    const requestURL = ServerAddress() + `api/officeHour/add/${officeHourID}`;
+    try {
+        const response = await fetch(requestURL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + userToken,
+            },
+        });
+        const data = await response.json();
+
+        if (response.status === 201) {  // adding OH is successful
+            console.log(data.message);
+            return true;
+        } else {
+            console.error(data.message);
+            return false; // not successful
+        }
+    } catch (error) {
+        console.error('An error occurred:', error);
+        return false;
+    }
+}
