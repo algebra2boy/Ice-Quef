41c206fbef2c4332e56dff13b05063948042d463
Haijie <64161293+ygbull@users.noreply.github.com>
2024-04-05 20:08:45 -0400

    Fix manage main page won't reload after new insertion

--- Ice_Quef/src/controllers/manage_page/DefaultController.js
+++ Ice_Quef/src/controllers/manage_page/DefaultController.js
@@ -1,37 +1,40 @@
-import React, { useEffect, useState, useContext } from 'react';
-import { ManagePageDefault } from '../../views/manage_page/Default';
-import { GetUserOfficeHour } from '../../models/RegisterModel';
-import { UserContext } from '../../props/UserInfo';
-import { LoadingPage } from '../../component/LoadingPage';
+import React, {useEffect, useState, useContext} from 'react';
+import {ManagePageDefault} from '../../views/manage_page/Default';
+import {GetUserOfficeHour} from '../../models/RegisterModel';
+import {UserContext} from '../../props/UserInfo';
+import {LoadingPage} from '../../component/LoadingPage';
+import {useOfficeHourUpdate} from "../../props/OfficeHourContext";
 
 export function ManagePageDefaultController() {
-  const user = useContext(UserContext);
-  const userEmail = user.email; // get user email address (account name)
+    const user = useContext(UserContext);
+    const userEmail = user.email; // get user email address (account name)
 
-  // The list of office hour that the student has currently enrolled
-  const [officeHour, setOfficeHour] = useState([]);
-  const [isLoading, setIsLoading] = useState(true); // loading indicator
+    const {updateTrigger} = useOfficeHourUpdate();
 
-  useEffect(() => {
-    const fetchUserOfficeHour = async () => {
-      try {
-        setIsLoading(true); // Before the fetch starts
-        const officeHours = await GetUserOfficeHour(userEmail);
-        setOfficeHour(officeHours);
-        // console.log(officeHours);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setIsLoading(false); // fetch is complete or if there is an error
-      }
-    };
+    // The list of office hour that the student has currently enrolled
+    const [officeHour, setOfficeHour] = useState([]);
+    const [isLoading, setIsLoading] = useState(true); // loading indicator
 
-    fetchUserOfficeHour();
-  }, []);
+    useEffect(() => {
+        const fetchUserOfficeHour = async () => {
+            try {
+                setIsLoading(true); // Before the fetch starts
+                const officeHours = await GetUserOfficeHour(userEmail);
+                setOfficeHour(officeHours);
+                // console.log(officeHours);
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setIsLoading(false); // fetch is complete or if there is an error
+            }
+        };
 
-  if (isLoading) {
-    return <LoadingPage text="Loading office hours..." />;
-  }
+        fetchUserOfficeHour();
+    }, [updateTrigger]);
 
-  return <ManagePageDefault ohList={officeHour} />;
+    if (isLoading) {
+        return <LoadingPage text="Loading office hours..."/>;
+    }
+
+    return <ManagePageDefault ohList={officeHour}/>;
 }
