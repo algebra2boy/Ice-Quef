1e279e2f4d970bf9da44505d35047025c9b39bf3
JianxinLin28 <jianxinlin@umass.edu>
2024-04-14 19:43:47 -0400

    Added login error message display

---
+++ src/props/LoginContext.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { createContext } from 'react';
+import { resetNavigatorTabIndex } from './NavigatorTabIndexController';
+
+export const LoginContext = createContext(null);
+
+export const LoginProvider = ({ children }) => {
+  const [logStatus, setLogin] = React.useState(0);
+
+  const updateLogStatus = () => {
+    setLogin(Date.now()); // timestamp for a unique value...idk if hash works too here
+    resetNavigatorTabIndex();
+  };
+
+  return (
+    <LoginContext.Provider value={{ logStatus, updateLogStatus }}>
+      {children}
+    </LoginContext.Provider>
+  );
+};
