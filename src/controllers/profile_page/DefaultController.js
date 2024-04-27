d0370505d949d877298c8830ef4355a064faa063
Kolyn090 <78561131+Kolyn090@users.noreply.github.com>
2024-04-27 15:39:05 -0400

    Added documentation for profile page controllers

--- src/controllers/profile_page/DefaultController.js
+++ src/controllers/profile_page/DefaultController.js
@@ -1,5 +1,10 @@
 import { ProfilePageDefault } from '../../views/profile_page/Default';
 
+/**
+ * The controller for the default profile page.
+ * 
+ * @returns { ReactElement } The default profile page
+ */
 export function ProfilePageDefaultController() {
   return <ProfilePageDefault />;
 }
