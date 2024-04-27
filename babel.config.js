da5ad0bcf0d073d9e3e7f6ee3e90e67c0f0c2222
JianxinLin28 <jianxinlin@umass.edu>
2024-04-27 19:58:37 -0400

    Switched testing lib

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,12 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    overrides: [
+      {
+        // Test files only
+        files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+        extends: ['plugin:testing-library/react']
+      }
+    ]
   };
 };
